09984c17c1f44bc062abf5e37af6425076568a49
Shane <11017547+Shanomac99@users.noreply.github.com>
2024-11-23 22:08:51 -0500

    Initial Converter Data Grab
    Add intial easy to grab data

--- Wiki/Converter/convert.py
+++ Wiki/Converter/convert.py
@@ -1,7 +1,45 @@
 import json
 
+convertedObject = {}
+
 # Read current version JSON file
 with open('completed_output/_Current Version/finished.json', "r") as file:
     CurrentJSON = json.load(file)
 
-print(CurrentJSON["Units"]["ADF_LO_CombinedArms"])
+    UNITS = CurrentJSON["Units"]
+    
+    # Loop through layer
+    for layer in CurrentJSON["Maps"]:
+        
+        # Build object
+        layerObject = {}
+
+        # ----- Direct Grabs ------
+        layerObject["gamemode"] = layer["gamemode"]
+        layerObject["layerVersion"] = layer["layerVersion"]
+        layerObject["heliAltThreshold"] = layer["heliAltThreshold"]
+        layerObject["mapSize"] = layer["mapSize"]
+
+        # ----- Indirect Grabs ------
+        # Team 1
+        layerObject["defaultTeam1"] = layer["teamConfigs"]["team1"]["defaultFactionUnit"]
+
+        # Check if the default team is present in units. It's possible an error in the extraction could result in it not appearing
+        if layerObject["defaultTeam1"] in UNITS:
+            layerObject["defaultTeam1Faction"] = UNITS[layerObject["defaultTeam1"]]["shortName"]
+        else:
+            print("Error, missing default team:")
+            print(layerObject["defaultTeam1"])
+        
+        layerObject["defaultTeam1Tickets"] = layer["teamConfigs"]["team1"]["tickets"]
+
+        # Team 2
+        layerObject["defaultTeam2"] = layer["teamConfigs"]["team2"]["defaultFactionUnit"]
+
+        if layerObject["defaultTeam2"] in UNITS:
+            layerObject["defaultTeam2Faction"] = UNITS[layerObject["defaultTeam2"]]["shortName"]
+        else:
+            print("Error, missing default team:")
+            print(layerObject["defaultTeam2"])
+
+        layerObject["defaultTeam2Tickets"] = layer["teamConfigs"]["team2"]["tickets"]
